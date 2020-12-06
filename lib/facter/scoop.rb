6a5b0ee7c72e3fb7b2900a3ff3826aca6756b734
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-12-06 23:29:31 +0100

    Run rubocop
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- lib/facter/scoop.rb
+++ lib/facter/scoop.rb
@@ -1,23 +1,23 @@
-Facter.add("scoop") do
-  confine :osfamily => :windows
+Facter.add('scoop') do
+  confine osfamily: :windows
   setcode do
-    buckets = Facter::Core::Execution.exec("scoop bucket list").split(/\r?\n/)
-  
-    package_list = Facter::Core::Execution.exec("scoop export").split(/\r?\n/)
-        
+    buckets = Facter::Core::Execution.exec('scoop bucket list').split(%r{\r?\n})
+
+    package_list = Facter::Core::Execution.exec('scoop export').split(%r{\r?\n})
+
     packages = {}
-    package_list.each do |line|        
+    package_list.each do |line|
       name, version, bucket, _ignore = line.strip.split(' ')
       packages[name] = {
         name: name,
         version: version,
-        bucket: bucket.gsub(/[\[\]]/)
+        bucket: bucket.gsub(%r{[\[\]]}),
       }
-      end
+    end
 
-      {
-        buckets: buckets,
-        packages: packages,
-      }
+    {
+      buckets: buckets,
+      packages: packages,
+    }
   end
 end
