1b010ce445a3343a8c8ee7c6efb7fbec8e0275a0
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2020-12-08 12:31:38 +0100

    Switch to the Machine scope
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- files/install.ps1
+++ files/install.ps1
@@ -1,4 +1,4 @@
 set-executionpolicy remotesigned -scope currentuser
-[environment]::setEnvironmentVariable("SCOOP", "C:\Applications\Scoop", "User")
+[environment]::setEnvironmentVariable("SCOOP", "C:\Applications\Scoop", "Machine")
 $env:SCOOP='C:\Applications\Scoop' # with this we don't need to close and reopen the console
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString("https://get.scoop.sh")
